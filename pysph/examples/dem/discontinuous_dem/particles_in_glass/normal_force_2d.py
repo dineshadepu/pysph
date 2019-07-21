dd51294037196def1fa141965dfa7054e667df6a
dineshadepu <adepu.dinesh.a@gmail.com>
2019-07-21 10:06:56 +0530

    Write up APS. Setup DEM examples.

---
+++ pysph/examples/dem/discontinuous_dem/particles_in_glass/normal_force_2d.py
@@ -0,0 +1,145 @@
+from __future__ import print_function
+import numpy as np
+
+# PySPH base and carray imports
+from pysph.base.kernels import CubicSpline
+from pysph.solver.solver import Solver
+
+from pysph.solver.application import Application
+from pysph.dem.discontinuous_dem.dem_nonlinear import EPECIntegratorMultiStage
+from pysph.sph.rigid_body import BodyForce
+from pysph.dem.discontinuous_dem.dem_linear import (
+    get_particle_array_dem_linear, LinearDEMNoRotationScheme,
+    UpdateTangentialContactsNoRotation, LinearPWFDEMNoRotationStage1,
+    LinearPWFDEMNoRotationStage2, RK2StepLinearDEMNoRotation,
+    UpdateTangentialContactsWallNoRotation, LinearPPFDEMNoRotationStage1,
+    LinearPPFDEMNoRotationStage2)
+from pysph.sph.scheme import SchemeChooser
+from pysph.base.utils import get_particle_array_rigid_body
+from pysph.base.utils import get_particle_array
+from pysph.tools.geometry import get_2d_tank, get_2d_block
+from pysph.sph.equation import Group, MultiStageEquations
+
+
+class ParticlesinGlass2d(Application):
+    def __init__(self, theta=0.):
+        self.theta = theta
+        super(ParticlesinGlass2d, self).__init__()
+
+    def initialize(self):
+        self.dx = 0.05
+        self.dt = 1e-4
+        self.tf = 5
+        self.dim = 2
+        self.en = 0.1
+        self.kn = 1e5
+        # friction coefficient
+        self.mu = 0.5
+        self.gy = -9.81
+        self.seval = None
+
+    def create_particles(self):
+        # create 2d wall with particles
+        dx = self.dx
+        xw, yw = np.mgrid[-10.*dx:10*dx:dx, -2*dx:0.:dx]
+        xw = xw.ravel()
+        yw = yw.ravel()
+        rho = 2699.
+        wall = get_particle_array(x=xw, y=yw, rho=rho, name="wall")
+        wall.add_property('dem_id', type='int')
+        wall.dem_id[:] = 0
+
+        # create bunch of particle
+        xp, yp = np.mgrid[-5*dx:5*dx:dx, dx:5.*dx:dx]
+
+        rad_s = np.ones_like(xp) * self.dx / 2.
+        rho = 2500
+        m = rho * rad_s**3.
+        inertia = m * 2. * rad_s**2. / 10.
+        m_inverse = 1. / m
+        I_inverse = 1. / inertia
+        sand = get_particle_array_dem_linear(
+            x=xp, y=yp, m=m, I_inverse=I_inverse, m_inverse=m_inverse,
+            rad_s=rad_s, dem_id=1, h=1.2 * self.dx / 2., name="sand")
+
+        return [wall, sand]
+
+    def create_equations(self):
+        eq1 = [
+            Group(
+                equations=[
+                    BodyForce(dest='sand', sources=None, gx=0.0, gy=-9.81,
+                              gz=0.0),
+                    LPPNForce(dest='sand', sources=['wall', 'sand'],
+                              kn=self.kn, mu=0.5, en=0.5),
+                ], real=False, update_nnps=False, iterate=False,
+                max_iterations=1, min_iterations=0, pre=None, post=None)
+        ]
+        eq2 = [
+            Group(
+                equations=[
+                    BodyForce(dest='sand', sources=None, gx=0.0, gy=-9.81,
+                              gz=0.0),
+                    LinearPWFDEMNoRotationStage2(dest='sand', sources=['wall'],
+                                                 kn=self.kn, mu=0.5, en=0.5),
+                    LinearPPFDEMNoRotationStage2(dest='sand', sources=['sand'],
+                                                 kn=self.kn, mu=0.5, en=0.5)
+                ], real=False, update_nnps=False, iterate=False,
+                max_iterations=1, min_iterations=0, pre=None, post=None)
+        ]
+
+        return MultiStageEquations([eq1, eq2])
+
+    def create_solver(self):
+        kernel = CubicSpline(dim=self.dim)
+
+        integrator = EPECIntegratorMultiStage(
+            sand=RK2StepLinearDEMNoRotation())
+
+        dt = self.dt
+        tf = self.tf
+        solver = Solver(kernel=kernel, dim=self.dim, integrator=integrator,
+                        dt=dt, tf=tf)
+        solver.set_disable_output(True)
+        return solver
+
+    def _make_accel_eval(self, equations, pa_arrays):
+        from pysph.tools.sph_evaluator import SPHEvaluator
+        if self.seval is None:
+            kernel = CubicSpline(dim=self.dim)
+            seval = SPHEvaluator(arrays=pa_arrays, equations=equations,
+                                 dim=self.dim, kernel=kernel)
+            self.seval = seval
+            return self.seval
+        else:
+            return self.seval
+
+    def post_step(self, solver):
+        t = solver.t
+        dt = solver.dt
+        eqs1 = [
+            Group(equations=[
+                UpdateTangentialContactsWallNoRotation(dest='sand',
+                                                       sources=['wall']),
+                UpdateTangentialContactsNoRotation(dest='sand',
+                                                   sources=['sand'])
+            ])
+        ]
+        arrays = self.particles
+        a_eval = self._make_accel_eval(eqs1, arrays)
+
+        # When
+        a_eval.evaluate(t, dt)
+
+    def customize_output(self):
+        self._mayavi_config('''
+        b = particle_arrays['sand']
+        b.plot.glyph.glyph_source.glyph_source = b.plot.glyph.glyph_source.glyph_dict['sphere_source']
+        b.plot.glyph.glyph_source.glyph_source.radius = {s_rad}
+        b.scalar = 'fy'
+        '''.format(s_rad=self.dx/2.))
+
+
+if __name__ == '__main__':
+    app = ParticlesinGlass2d()
+    app.run()
