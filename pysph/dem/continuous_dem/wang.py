54dc4c46e984c38bf73899c0a31cfee0e11c6700
dineshadepu <adepu.dinesh.a@gmail.com>
2019-08-25 12:32:04 +0530

    Wang implementation in progress. (Bonded DEM)

--- pysph/dem/continuous_dem/wang.py
+++ pysph/dem/continuous_dem/wang.py
@@ -1,13 +1,22 @@
+"""
+This is a scheme of modeling continuous material using particles from
+
+A new algorithm to model the dynamics of 3-D bonded rigid
+bodies with rotations
+
+"""
 from pysph.sph.equation import Equation
 from pysph.base.utils import get_particle_array
 from pysph.sph.integrator_step import IntegratorStep
 from compyle.api import declare
-from math import sqrt, asin, sin, cos, log
+from math import sqrt, asin, sin, acos, log
 import numpy as np
 from pysph.sph.scheme import Scheme
 from pysph.sph.equation import Group, MultiStageEquations
 from pysph.sph.integrator import EulerIntegrator
 from pysph.base.kernels import CubicSpline
+from pysph.sph.quaternion import (q_inverse_vec_q, dot_product, cross_product,
+                                  magnitude, quaternion_multiplication)
 
 
 def get_particle_array_bonded_dem_wang(constants=None, **props):
@@ -18,7 +27,8 @@ def get_particle_array_bonded_dem_wang(constants=None, **props):
     dem_props = [
         'wx', 'wy', 'wz', 'fx', 'fy', 'fz', 'torx', 'tory', 'torz', 'rad_s',
         'm_inverse', 'I_inverse', 'u0', 'v0', 'w0', 'wx0', 'wy0', 'wz0', 'x0',
-        'y0', 'z0'
+        'y0', 'z0', 'q0', 'q1', 'q2', 'q3', 'q0_0', 'q1_0', 'q2_0', 'q3_0',
+        'qinv0', 'qinv1', 'qinv2', 'qinv3'
     ]
 
     dem_id = props.pop('dem_id', None)
@@ -39,6 +49,9 @@ def get_particle_array_bonded_dem_wang(constants=None, **props):
 
     pa.add_property('bc_total_contacts', type='int')
     pa.add_property('bc_rest_len', stride=bc_limit)
+    pa.add_property('bc_x_t0', stride=bc_limit)
+    pa.add_property('bc_y_t0', stride=bc_limit)
+    pa.add_property('bc_z_t0', stride=bc_limit)
     pa.add_property('bc_ft_x', stride=bc_limit)
     pa.add_property('bc_ft_y', stride=bc_limit)
     pa.add_property('bc_ft_z', stride=bc_limit)
@@ -79,7 +92,8 @@ class SetupContactsBC(Equation):
         super(SetupContactsBC, self).__init__(dest, sources)
 
     def loop(self, d_idx, s_idx, d_bc_total_contacts, d_bc_idx, d_bc_limit,
-             d_rad_s, d_bc_rest_len, s_rad_s, RIJ):
+             d_rad_s, d_bc_x_t0, d_bc_y_t0, d_bc_z_t0, d_x, d_y, d_z, s_x, s_y,
+             s_z, s_rad_s, RIJ):
         p = declare('int')
         p = d_bc_limit[0] * d_idx + d_bc_total_contacts[d_idx]
         fac = RIJ / (d_rad_s[d_idx] + s_rad_s[s_idx])
@@ -87,7 +101,11 @@ class SetupContactsBC(Equation):
             if 1. - self.beta < fac < 1 + self.beta:
                 d_bc_idx[p] = s_idx
                 d_bc_total_contacts[d_idx] += 1
-                d_bc_rest_len[p] = RIJ
+                # save the initial position of the contact
+                # with respect to the particle d_idx
+                d_bc_x_t0[p] = s_x[s_idx] - d_x[d_idx]
+                d_bc_y_t0[p] = s_y[s_idx] - d_y[d_idx]
+                d_bc_z_t0[p] = s_z[s_idx] - d_z[d_idx]
 
 
 class PotyondyIPForceStage1(Equation):
@@ -96,12 +114,39 @@ class PotyondyIPForceStage1(Equation):
         self.kt = kn / 2.
         super(PotyondyIPForceStage1, self).__init__(dest, sources)
 
-    def initialize(self, d_idx, d_bc_total_contacts, d_x, d_y, d_bc_ft_x,
+    def _get_helpers_(self):
+        return [q_inverse_vec_q, dot_product, cross_product, quaternion_multiplication]
+
+    def initialize(self, d_idx, d_bc_total_contacts, d_x, d_y, d_z, d_bc_ft_x,
                    d_bc_ft_y, d_bc_ft_z, d_bc_ft0_x, d_bc_ft0_y, d_bc_ft0_z,
                    d_bc_limit, d_bc_idx, d_bc_rest_len, d_fx, d_fy, d_u, d_v,
-                   dt):
+                   d_q0, d_q1, d_q2, d_q3, d_qinv0, d_qinv1, d_qinv2, d_qinv3,
+                   d_bc_x_t0, d_bc_y_t0, d_bc_z_t0, dt):
         p, q1, tot_ctcs, i, sidx = declare('int', 5)
-        xij = declare('matrix(3)')
+        rf = declare('matrix(3)')
+        rc = declare('matrix(3)')
+        r0 = declare('matrix(3)')
+        rc_cross_r0 = declare('matrix(3)')
+        rc_cross_rc_cross_r0 = declare('matrix(3)')
+        r0_cross_rc = declare('matrix(3)')
+        s_vec = declare('matrix(3)')
+        t_vec = declare('matrix(3)')
+        q_didx = declare('matrix(4)')
+        qinv_didx = declare('matrix(4)')
+        q_sidx = declare('matrix(4)')
+        g_not = declare('matrix(4)')
+        g = declare('matrix(4)')
+        h = declare('matrix(4)')
+        tmp_quat = declare('matrix(4)')
+
+        # all the force declarations
+        # normal force
+        fr = declare('matrix(3)')
+        # shear force's translational component
+        fs_t = declare('matrix(3)')
+
+        # torque due to  shear force's translational component
+        tors_t = declare('matrix(3)')
         # total number of contacts of particle i in destination
         tot_ctcs = d_bc_total_contacts[d_idx]
 
@@ -111,159 +156,144 @@ class PotyondyIPForceStage1(Equation):
         # ending index is q -1
         q1 = p + tot_ctcs
 
-        for i in range(p, q1):
-            sidx = d_bc_idx[i]
-            overlap = -1.
-            rij = 0.0
+        # save the quaternion of d_idx for easy computation
+        q_didx[0] = d_q0[d_idx]
+        q_didx[1] = d_q1[d_idx]
+        q_didx[2] = d_q2[d_idx]
+        q_didx[3] = d_q3[d_idx]
 
-            xij[0] = d_x[d_idx] - d_x[sidx]
-            xij[1] = d_y[d_idx] - d_y[sidx]
-            rij = sqrt(xij[0] * xij[0] + xij[1] * xij[1])
-
-            # normal vector from i to j
-            nji_x = xij[0] / rij
-            nji_y = xij[1] / rij
-
-            rinv = 1. / rij
-            # print("didx")
-            # print(d_idx)
-            # print("sidx")
-            # print(sidx)
-            # print("hi")
-            # print(d_bc_rest_len[i])
-            # check the particles are not on top of each other.
-            if rij > 0:
-                overlap = rij - d_bc_rest_len[i]
-
-            # print("rij")
-            # print(rij)
-            # print("overlap")
-            # print(overlap)
-
-            # relative velocity
-            vr_x = d_u[d_idx] - d_u[sidx]
-            vr_y = d_v[d_idx] - d_v[sidx]
-
-            # normal velocity magnitude
-            vr_dot_nij = vr_x * nji_x + vr_y * nji_y
-            vn_x = vr_dot_nij * nji_x
-            vn_y = vr_dot_nij * nji_y
-
-            # ---------- force computation starts ------------
-            d_fx[d_idx] -= self.kn * nji_x * overlap - vn_x * 10.
-            d_fy[d_idx] -= self.kn * nji_y * overlap - vn_y * 10.
-
-            # tangential force
-            ft_x = d_bc_ft_x[i]
-            ft_y = d_bc_ft_y[i]
-
-            # TODO
-            # check the coulomb criterion
-
-            # Add the tangential force
-            d_fx[d_idx] += ft_x
-            d_fy[d_idx] += ft_y
-
-            # --------- Tangential force -----------
-            # -----increment the tangential force---
-            # tangential velocity
-            vt_x = vr_x - vn_x
-            vt_y = vr_y - vn_y
-
-            # magnitude of the tangential velocity
-            # vt_magn = (vt_x * vt_x + vt_y * vt_y + vt_z * vt_z)**0.5
-
-            # get the incremental force
-            dtb2 = dt / 2.
-            d_bc_ft_x[i] += - self.kt * vt_x * dtb2 - vt_x * 1000.
-            d_bc_ft_y[i] += - self.kt * vt_y * dtb2 - vt_y * 1000.
-
-
-class PotyondyIPForceStage2(Equation):
-    def __init__(self, dest, sources, kn):
-        self.kn = kn
-        self.kt = kn / 2.
-        super(PotyondyIPForceStage2, self).__init__(dest, sources)
-
-    def initialize(self, d_idx, d_bc_total_contacts, d_x, d_y, d_bc_ft_x,
-                   d_bc_ft_y, d_bc_ft_z, d_bc_ft0_x, d_bc_ft0_y, d_bc_ft0_z,
-                   d_bc_limit, d_bc_idx, d_bc_rest_len, d_fx, d_fy, d_u, d_v, dt):
-        p, q1, tot_ctcs, i, sidx = declare('int', 5)
-        xij = declare('matrix(3)')
-        # total number of contacts of particle i in destination
-        tot_ctcs = d_bc_total_contacts[d_idx]
-
-        # d_idx has a range of tracking indices with sources
-        # starting index is p
-        p = d_idx * d_bc_limit[0]
-        # ending index is q -1
-        q1 = p + tot_ctcs
+        qinv_didx[0] = d_qinv0[d_idx]
+        qinv_didx[1] = d_qinv1[d_idx]
+        qinv_didx[2] = d_qinv2[d_idx]
+        qinv_didx[3] = d_qinv3[d_idx]
 
         for i in range(p, q1):
             sidx = d_bc_idx[i]
-            overlap = -1.
-            rij = 0.0
 
-            xij[0] = d_x[d_idx] - d_x[sidx]
-            xij[1] = d_y[d_idx] - d_y[sidx]
-            rij = sqrt(xij[0] * xij[0] + xij[1] * xij[1])
+            # save the quaternion of sidx for easy computation
+            q_sidx[0] = d_q0[sidx]
+            q_sidx[1] = d_q1[sidx]
+            q_sidx[2] = d_q2[sidx]
+            q_sidx[3] = d_q3[sidx]
 
-            # normal vector from i to j
-            nji_x = xij[0] / rij
-            nji_y = xij[1] / rij
+            # the relative position of particle sidx with respect
+            # to d_idx in global frame as well as d_idx body frame
+            # at time t=0. (This vector is same in both the frames
+            # as at time 0 both frames are same (1., 0., 0., 0.) )
+            # QUESTION: Should this vector be converted to the current
+            # orientation of the body frame?
+            r0[0] = d_bc_x_t0[i]
+            r0[1] = d_bc_y_t0[i]
+            r0[2] = d_bc_z_t0[i]
 
-            rinv = 1. / rij
-            # print("didx")
-            # print(d_idx)
-            # print("sidx")
-            # print(sidx)
-            # print("hi")
-            # print(d_bc_rest_len[i])
-            # check the particles are not on top of each other.
-            if rij > 0:
-                overlap = rij - d_bc_rest_len[i]
+            # the relative position of particle sidx with respect
+            # to d_idx in global frame
+            rf[0] = d_x[sidx] - d_x[d_idx]
+            rf[1] = d_y[sidx] - d_y[d_idx]
+            rf[2] = d_z[sidx] - d_z[d_idx]
 
-            # print("rij")
-            # print(rij)
-            # print("overlap")
-            # print(overlap)
+            # the relative position of particle sidx with respect
+            # to d_idx in body frame of particle d_idx
 
-            # relative velocity
-            vr_x = d_u[d_idx] - d_u[sidx]
-            vr_y = d_v[d_idx] - d_v[sidx]
+            # transform the global vector rf to local frame
+            # of particle d_idx by quaternion conversion
+            # after the function call we have `rc` to be the
+            # relative position of particle sidx with respect to
+            # particle d_idx in body frame of d_idx
+            # equation (11)
+            q_inverse_vec_q(rf, q_didx, qinv_didx, rc)
 
-            # normal velocity magnitude
-            vr_dot_nij = vr_x * nji_x + vr_y * nji_y
-            vn_x = vr_dot_nij * nji_x
-            vn_y = vr_dot_nij * nji_y
+            # ----------- computation of normal force -----------
+            # normal force acting on particle d_idx
+            # equation (12)
+            rc_magn = magnitude(rc)
+            r0_magn = magnitude(r0)
 
-            # ---------- force computation starts ------------
-            d_fx[d_idx] -= self.kn * nji_x * overlap - vn_x * 10.
-            d_fy[d_idx] -= self.kn * nji_y * overlap - vn_y * 10.
+            # change in length between particles d_idx and sidx
+            delta_r = rc_magn - r0_magn
+            # now compute the normal force from the change in length and the
+            # current direction of the connection
 
-            # tangential force
-            ft_x = d_bc_ft_x[i]
-            ft_y = d_bc_ft_y[i]
+            # There are a few points to note here, that the force computed is
+            # in body frame of d_idx. One needs to keep in mind that while
+            # using this force move the particle forward, he/she has to
+            # convert this force and use it.
+            tmp = self.kr * delta_r
+            fr[0] = tmp * rc[0] / rc_magn
+            fr[1] = tmp * rc[1] / rc_magn
+            fr[2] = tmp * rc[2] / rc_magn
+            # ----------- computation of normal force ends -----------
 
-            # TODO
-            # check the coulomb criterion
+            # -----------------------------------------------------
+            # ----------- computation of shearing force -----------
+            # -----------------------------------------------------
 
-            # Add the tangential force
-            d_fx[d_idx] += ft_x
-            d_fy[d_idx] += ft_y
+            # shearing force has two contributions, one is due to translational
+            # and rotational. First we will look at translational shear force
+            # then we will compute the rotational part.
+            # equation (13)
 
-            # --------- Tangential force -----------
-            # -----increment the tangential force---
-            # tangential velocity
-            vt_x = vr_x - vn_x
-            vt_y = vr_y - vn_y
+            # ----------- computation of shear force due to translation -----
+            # translational shear force acting on particle d_idx
+            # equation (14)
+            # compute angle gamma, between initial vector passing from
+            # d_idx to sidx (r0) and with vector passing from d_idx to sidx
+            # (rc) at current time.
+            gamma = acos(dot_product(r0, rc) / (r0_magn * rc_magn))
 
-            # magnitude of the tangential velocity
-            # vt_magn = (vt_x * vt_x + vt_y * vt_y + vt_z * vt_z)**0.5
+            # the direction of the translational shear force is s_vec
+            cross_product(rc, r0, rc_cross_r0)
+            cross_product(rc, rc_cross_r0, rc_cross_rc_cross_r0)
+            rc_c_rc_c_r0_magn = magnitude(rc_cross_rc_cross_r0)
 
-            # get the incremental force
-            d_bc_ft_x[i] = d_bc_ft0_x[i] - self.kt * vt_x * dt - vt_x * 1000.
-            d_bc_ft_y[i] = d_bc_ft0_y[i] - self.kt * vt_y * dt - vt_y * 1000.
+            s_vec[0] = rc_cross_rc_cross_r0[0] / rc_c_rc_c_r0_magn
+            s_vec[1] = rc_cross_rc_cross_r0[1] / rc_c_rc_c_r0_magn
+            s_vec[2] = rc_cross_rc_cross_r0[2] / rc_c_rc_c_r0_magn
+
+            # using gamma, vector and shear stiffness, the force results as
+            tmp = self.ks * r0_magn * gamma
+            fs_t[0] = tmp * s_vec[0]
+            fs_t[1] = tmp * s_vec[1]
+            fs_t[2] = tmp * s_vec[2]
+            fs_t_magn = magnitude(fs_t)
+
+            # since this force acts on the geometric center of the two body
+            # system of d_idx and sidx. It generates a moment or torqe on
+            # particle d_idx
+
+            # the direction of the torque by the translational shear force is
+            # t_vec
+            cross_product(r0, rc, r0_cross_rc)
+            r0_cross_rc_magn = magnitude(r0_cross_rc)
+
+            t_vec[0] = r0_cross_rc[0] / r0_cross_rc_magn
+            t_vec[1] = r0_cross_rc[1] / r0_cross_rc_magn
+            t_vec[2] = r0_cross_rc[2] / r0_cross_rc_magn
+
+            tmp = 0.5 * rc_magn * fs_t_magn
+            tors_t[0] = tmp * t_vec[0]
+            tors_t[1] = tmp * t_vec[1]
+            tors_t[2] = tmp * t_vec[2]
+
+            # ------ computation of shear force due to translation ends -----
+
+            # ------- computation of shear force due to rotation ----------
+            # the relative rotation from body frame of d_idx to the body frame
+            # of sidx (X2Y2Z2 to X1Y1Z1) g^0 = q_inv * p (say g_not)
+            # where q is quaternion of d_idx particle and p for sidx particle
+
+            # lets first compute g_not
+            quaternion_multiplication(qinv_didx, q_sidx, g_not)
+
+            # now by using a quaternion (h), which specifies the rotation from
+            # X2Y2Z2 to X2'Y2'Z2', we can compute the quaternion (g) which
+            # specifies the rotation from X2'Y2'Z2' to X1'Y1'Z1', and given by
+            # g = h_inv g_not h
+            # first compute h
+            tmp = 0.5**0.5 *
+            h[0] = one_root_2 * ((rc_magn + rc[2]) / 1)**0.5
+            quaternion_multiplication()
+
 
 
 class RK2StepPotyondy(IntegratorStep):
---
+++ pysph/sph/quaternion.py
@@ -0,0 +1,59 @@
+from compyle.api import declare
+
+
+def q_inverse_vec_q(rf=[1.0, 0.0], q=[1.0, 0.0], qinv=[1.0, 0.0],
+                    rc=[0.0, 0.0]):
+    """Multiply a square matrix with a vector.
+
+    Parameters
+    ----------
+
+    a: list
+    b: list
+    n: int : number of rows/columns
+    result: list
+    """
+    i, j = declare('int', 2)
+    for i in range(n):
+        s = 0.0
+        for j in range(n):
+            s += a[n * i + j] * b[j]
+        result[i] = s
+
+
+def cross_product(a, b, res):
+    res[0] = a[1] * b[2] - a[2] * b[1]
+    res[1] = a[2] * b[0] - a[0] * b[2]
+    res[2] = a[0] * b[1] - a[1] * b[0]
+
+
+def dot_product(a, b):
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+def magnitude(a):
+    return (a[0] * a[0] + a[1] * a[1] + a[2] * a[2])**0.5
+
+
+def quaternion_multiplication(p, q, res):
+    """Parameters
+    ----------
+    p   : [float]
+          An array of length four
+    q   : [float]
+          An array of length four
+    res : [float]
+          An array of length four
+    Here `p` is a quaternion. i.e., p = [p.w, p.x, p.y, p.z]. And q is an
+    another quaternion.
+    This function is used to compute the rate of change of orientation
+    when orientation is represented in terms of a quaternion. When the
+    angular velocity is represented in terms of global frame
+    \frac{dq}{dt} = \frac{1}{2} omega q
+    http://www.ams.stonybrook.edu/~coutsias/papers/rrr.pdf
+    see equation 8
+    """
+    res[0] = (p[0] * q[0] - p[1] * q[1] - p[2] * q[2] - p[3] * q[3])
+    res[1] = (p[0] * q[1] + q[0] * p[1] + p[2] * q[3] - p[3] * q[2])
+    res[2] = (p[0] * q[2] + q[0] * p[2] + p[3] * q[1] - p[1] * q[3])
+    res[3] = (p[0] * q[3] + q[0] * p[3] + p[1] * q[2] - p[2] * q[1])
